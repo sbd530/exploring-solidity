0f72e16a2cb65fc4643b8df4c63117407f1dd8cc
Byungdon Yoon <byungdon92@gmail.com>
2022-05-05 22:44:35 +0900

    hardhat + uniswapV3

--- DeFi/uniswapV3-twap/hardhat.config.js
+++ DeFi/uniswapV3-twap/hardhat.config.js
@@ -17,5 +17,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.7.6",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+  }
 };
