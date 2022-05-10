728589c1ca52ce766f6a69f64282fe6eee7b7302
Byungdon Yoon <byungdon92@gmail.com>
2022-05-10 23:59:23 +0900

    20220510

--- decentra_twitter/contracts/Decentratwitter.sol
+++ decentra_twitter/contracts/Decentratwitter.sol
@@ -6,5 +6,6 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract Decentratwitter is ERC721URIStorage {
+    //
     constructor() ERC721("Decentratwitter", "DAPP") {}
 }
